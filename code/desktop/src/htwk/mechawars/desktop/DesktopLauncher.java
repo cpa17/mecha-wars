d10d1ac70070a1b90d78aecbf399f4fbd26b21a7
Till Mahlburg <till.mahlburg@htwk-leipzig.de>
2021-06-10 17:41:00 +0200

    fix formatting

--- code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
+++ code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
@@ -4,10 +4,16 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import htwk.mechawars.MechaWars;
 
+/**
+* Class containing the runner for the desktop frontend.
+*/
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(800, 480);
-		new Lwjgl3Application(new MechaWars(), config);
-	}
+    /**
+    * Main class, running the desktop frontend.
+    */
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(800, 480);
+        new Lwjgl3Application(new MechaWars(), config);
+    }
 }
