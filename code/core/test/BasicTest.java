fe4d1eea4cb6bd2c76d909a59da854d2ccac3785
Till Mahlburg <till.mahlburg@htwk-leipzig.de>
2021-06-10 15:59:07 +0200

    setup JUnit5

---
+++ code/core/test/BasicTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class BasicTest {
+    @Test
+    public void testBasic() {
+        assertEquals(2, 1 + 1);
+    }
+}
