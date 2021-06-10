25c1f87ce32287fde7b795102452040a1d77411d
Till Mahlburg <till.mahlburg@htwk-leipzig.de>
2021-06-10 14:55:55 +0200

    initial libGDX setup

---
+++ code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package htwk.mechawars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import htwk.mechawars.MechaWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(800, 480);
+		new Lwjgl3Application(new MechaWars(), config);
+	}
+}
