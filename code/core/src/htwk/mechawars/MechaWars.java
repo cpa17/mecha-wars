d66f5053a0d2b6f5ec345b57983e48b33c865ab3
Antonia Swiridoff <antonia.swiridoff@stud.htwk-leipzig.de>
2021-06-12 14:02:33 +0200

    added playfield

--- code/core/src/htwk/mechawars/MechaWars.java
+++ code/core/src/htwk/mechawars/MechaWars.java
@@ -1,35 +1,62 @@
 package htwk.mechawars;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
+//import com.badlogic.gdx.math.Rectangle;
 
 
 /**
 * Main game class.
 */
 public class MechaWars extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Texture industrialTile;
+    private Rectangle recIndustrialTile;
+    private int cameraWidth = 1280;
+    private int cameraHeight = 720;
 
     @Override
     public void create() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, cameraWidth, cameraHeight);
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        industrialTile = new Texture("industrialTile.png");
+//        recIndustrialTile = new Rectangle();
+//        recIndustrialTile.width = 32;
+//        recIndustrialTile.height = 32;
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(1, 0, 0, 1);
+        ScreenUtils.clear(1, 1, 1, 1);
+        camera.update();
+//        batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(img, 0, 0);
+        int x = 0;
+        int i = 0;
+        while (i < 12) {
+            int y = 0;
+            int j = 0;
+            while (j < 12) {
+                batch.draw(industrialTile, x, y);
+                y = y + (cameraHeight / 12);
+                j++;
+            }
+            x = x + (cameraHeight / 12);
+            i++;
+        }
         batch.end();
     }
 
     @Override
     public void dispose() {
+        industrialTile.dispose();
         batch.dispose();
-        img.dispose();
     }
 }
--- code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
+++ code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
@@ -13,7 +13,7 @@ public class DesktopLauncher {
     */
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setWindowedMode(800, 480);
+        config.setWindowedMode(1280, 720);
         new Lwjgl3Application(new MechaWars(), config);
     }
 }
