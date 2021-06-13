3ee05572752c81deb4b61018c20b8473240269af
Antonia Swiridoff <antonia.swiridoff@stud.htwk-leipzig.de>
2021-06-13 12:58:35 +0200

    Added stage with clickable buttons, deleted old buttons

--- code/core/src/htwk/mechawars/GameScreen.java
+++ code/core/src/htwk/mechawars/GameScreen.java
@@ -5,28 +5,78 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GameScreen implements Screen {
 
     final MechaWars game;
 
     Texture industrialTile;
-    Texture backButton;
-    Texture continueButton;
     OrthographicCamera camera;
+    Stage stage;
     private static final int cameraWidth = 1280;
     private static final int cameraHeight = 720;
 
     public GameScreen(final MechaWars game) {
         this.game = game;
         industrialTile = new Texture("industrialTile.png");
-        backButton = new Texture("backButton.png");
-        continueButton = new Texture("continueButton.png");
         camera = new OrthographicCamera();
         camera.setToOrtho(false, cameraWidth, cameraHeight);
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+        addButtonsToStage(skin);
     }
 
+    public void addButtonsToStage(Skin skin) {
+        int startExecutionButtonX = cameraHeight + (cameraWidth - cameraHeight)/3 -64;
+        int startExecutionButtonY = cameraHeight - 100;
+        int endGameButtonX = cameraHeight + (((cameraWidth-cameraHeight)*2)/3)-64;
+        int endGameButtonY = cameraHeight - 100;
+
+        Button startExecutionButton = new TextButton("Start Execution", skin);
+        Button endGameButton = new TextButton("End Game", skin);
+
+        startExecutionButton.setSize(128, 43);
+        endGameButton.setSize(128, 43);
+
+        startExecutionButton.setPosition(startExecutionButtonX,startExecutionButtonY);
+        endGameButton.setPosition(endGameButtonX,endGameButtonY);
+
+        startExecutionButton.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("startExecutionButton touchUp ausgelöst!");
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("startExecutionButton touchDown ausgelöst!");
+                return true;
+            }
+        });
+
+        endGameButton.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("endGameButton touchUp ausgelöst!");
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("endGameButton touchDown ausgelöst!");
+                return true;
+            }
+        });
+
+        stage.addActor(startExecutionButton);
+        stage.addActor(endGameButton);
+    }
 
     @Override
     public void show() {
@@ -40,8 +90,9 @@ public class GameScreen implements Screen {
  //       game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
         drawPlayingField();
-        drawButtons();
         game.batch.end();
+        stage.act();
+        stage.draw();
     }
 
     public void drawPlayingField(){
@@ -58,25 +109,6 @@ public class GameScreen implements Screen {
             x = x + (cameraHeight / 12);
             i++;
         }
-    }
-
-    public void drawButtons(){
-        int continueButtonX = cameraHeight + (cameraWidth - cameraHeight)/3 -64;
-        int backButtonX = cameraHeight + (((cameraWidth-cameraHeight)*2)/3)-64;
-        int continueButtonY = cameraHeight - 100;
-        int backButtonY = cameraHeight - 100;
-        /*
-        if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-            if (((Gdx.input.getX() <= (continueButtonX + 128)) && (Gdx.input.getX() >= continueButtonX)) && ((Gdx.input.getY() <= (continueButtonY + 43)) && (Gdx.input.getY() >= continueButtonY))) {
-                System.out.println("Continue gedrückt!");
-            }
-            if (((Gdx.input.getX() <= (backButtonX + 128)) && (Gdx.input.getX() >= backButtonX)) && ((Gdx.input.getY() <= (backButtonY + 43)) && (Gdx.input.getY() >= backButtonY))) {
-                System.out.println("Back gedrückt!");
-            }
-        }
-        */
-        game.batch.draw(continueButton, continueButtonX, continueButtonY);
-        game.batch.draw(backButton, backButtonX, backButtonY);
     }
 
     @Override
@@ -101,6 +133,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void hide() {
+
     }
 
 }
