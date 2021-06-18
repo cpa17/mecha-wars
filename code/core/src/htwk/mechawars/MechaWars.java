038aa58e2d04f488aad7ecb0627ae0fcb70eb254
mfiedler <mfiedler@stud.htwk-leipzig.de>
2021-06-18 14:44:07 +0200

    Pipeline fixed and small changes

--- code/core/src/htwk/mechawars/MechaWars.java
+++ code/core/src/htwk/mechawars/MechaWars.java
@@ -3,30 +3,28 @@ package htwk.mechawars;
 import com.badlogic.gdx.Game;
 
 /**
-* Main game class.
-*/
+ * Main game class.
+ */
 public class MechaWars extends Game {
-	
-	
-	/**
-	 * Method & Attribute for skipping the MainMenu when executing the Program
-	 */
-	private static boolean isSkip = false; //Variable zum Skippen
-	
-	public static void setSkip(boolean skip){
-		isSkip=skip;
-	}
-	
-	/**
-	 * Method to start either from the main menu or the game screen
-	 */
+
+    /**
+     * Method & Attribute for skipping the MainMenu when executing the Program.
+     */
+    private static boolean isSkip = false; 
+
+    public static void setSkip(boolean skip) {
+        isSkip = skip;
+    }
+
+    /**
+     * Method to start either from the main menu or the game screen.
+     */
     @Override
     public void create() {
-    	if(isSkip==true) {
-    		this.setScreen(new GameScreen());
-        }
-        else {
-        	this.setScreen(new MainMenu(this));
+        if (isSkip == true) {
+            this.setScreen(new GameScreen());
+        } else {
+            this.setScreen(new MainMenu(this));
         }
     }
 
--- code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
+++ code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
@@ -3,52 +3,52 @@ package htwk.mechawars.desktop;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import htwk.mechawars.MechaWars;
-
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-
 /**
-* Class containing the runner for the desktop frontend.
-*/
-@Command(
-version = "Mecha Wars 1.0",
-header = "%Mecha Wars%n",
-mixinStandardHelpOptions = true)
-public class DesktopLauncher implements Runnable{
-	
-	@Option(names = { "-s", "--skip"}, description="Starts the Game, without showing the MainMenu at first." )
-	boolean skip;
-	
-	/**
-	* Main class, for the new CommandLine
-	*/
-    public static void main(String[] args) {
-	System.exit(new CommandLine(new DesktopLauncher()).execute(args));
-    }
-    
-    /**
-	* Method, running the desktop frontend.
-	*/
-    @Override 
-	public void run() {
-	if(skip == true) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(1280, 720);
-		MechaWars.setSkip(skip);
-		new Lwjgl3Application(new MechaWars(), config);
-	}
-	else {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(1280, 720);
-		new Lwjgl3Application(new MechaWars(), config);
-	}
-}
-}
-
-/**
- * JAR: .\gradlew.bat desktop:dist in der Konsole, im Code Ordner
- * die JAR-File liegt dann unter -> code -> desktop -> build -> libs
+ * Class containing the runner for the desktop frontend.
+ * Write .\gradlew.bat desktop:dist in your console, 
+ * in the writer code in order to provide the jar-file.
+ * One can find the jar-file under mecha-wars -> code -> desktop -> build -> libs.
+ * Execute the file in your shell with the command 
+ * java -jar mw.jar, get all options with the command java -jar mw.jar -h.
  */
+
+@Command(
+        version = "Mecha Wars 1.0",
+        header = "Mecha Wars",
+        mixinStandardHelpOptions = true)
+public class DesktopLauncher implements Runnable {
+
+    @Option(names = { "-s", "--skip" }, 
+            description = "Starts the Game, without showing the MainMenu at first.")
+    boolean skip;
+
+    /**
+     * Main class, for the new CommandLine.
+     */
+    public static void main(String[] args) {
+        System.exit(new CommandLine(new DesktopLauncher()).execute(args));
+    }
+
+    /**
+     * Method, running the desktop frontend.
+     */
+    @Override 
+    public void run() {
+        if (skip == true) {
+            Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+            config.setWindowedMode(1280, 720);
+            MechaWars.setSkip(skip);
+            new Lwjgl3Application(new MechaWars(), config);
+        } else {
+            Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+            config.setWindowedMode(1280, 720);
+            new Lwjgl3Application(new MechaWars(), config);
+        }
+    }
+}
+
 
