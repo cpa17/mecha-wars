0ffc4b5ba84fbd37e03d49f8b6cc779f6f8f89a7
kblechsc <konstantin.blechschmidt@stud.htwk-leipzig.de>
2021-06-19 18:46:47 +0200

    Test 2 for cards, comments for both card-tests

--- code/core/test/BasicTest.java
+++ code/core/test/BasicTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import htwk.mechawars.cards.Card;
@@ -11,6 +13,9 @@ class BasicTest {
         assertEquals(2, 1 + 1);
     }
     
+    /**
+     * Tests if the movCount is only -1, 1, 2 or 3
+     */
     public void kartenAnzahlTest() {
         Card[] testCards = new Card[84];
         
@@ -22,6 +27,22 @@ class BasicTest {
             assertEquals(1, x, "1 Stimmt" + (i+1) + ". Karte");
             assertEquals(2, x, "2 Stimmt" + (i+1) + ". Karte");
             assertEquals(3, x, "3 Stimmt" + (i+1) + ". Karte");
+        }
+    }
+    
+    /**
+     * Tests if the names of the Buttons are the Strings of the enum "Name"
+     */
+    public void testName() {
+        Card[] testCards = new Card[84];
+        for (int i = 0; i < 84; i+=1) {
+            assertTrue(testCards[i].getCardAttributeName().get_Name() == "1 Vor" ||
+                       testCards[i].getCardAttributeName().get_Name() == "2 Vor" ||
+                       testCards[i].getCardAttributeName().get_Name() == "3 Vor" ||
+                       testCards[i].getCardAttributeName().get_Name() == "Rueckwaerts" ||
+                       testCards[i].getCardAttributeName().get_Name() == "Rechtsdrehung" ||
+                       testCards[i].getCardAttributeName().get_Name() == "Kehrtwendung" ||
+                       testCards[i].getCardAttributeName().get_Name() == "Linksdrehung" );
         }
     }
 }
