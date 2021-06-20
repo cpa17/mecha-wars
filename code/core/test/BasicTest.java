de626b6efc62b4a721fbdb32f4a100f446591379
tkretzsc <tim_michael.kretzschmar@stud.htwk-leipzig.de>
2021-06-20 11:54:19 +0200

    Testfehler behoben

--- code/core/test/BasicTest.java
+++ code/core/test/BasicTest.java
@@ -16,6 +16,7 @@ class BasicTest {
     /**
      * Tests if the movCount is only -1, 1, 2 or 3
      */
+    @Test
     public void kartenAnzahlTest() {
         Card[] testCards = new Card[84];
         
@@ -23,18 +24,19 @@ class BasicTest {
         
         for(int i=0; i<84; i+=1) {
             int x = testCards[i].getCardAttributeMovCount();
-            assertEquals(-1, x, "-1 Stimmt" + (i+1) + ". Karte");
-            assertEquals(1, x, "1 Stimmt" + (i+1) + ". Karte");
-            assertEquals(2, x, "2 Stimmt" + (i+1) + ". Karte");
-            assertEquals(3, x, "3 Stimmt" + (i+1) + ". Karte");
+            assertTrue(-1 == x || 1 == x || 2 == x || 3 == x);
         }
     }
     
     /**
      * Tests if the names of the Buttons are the Strings of the enum "Name"
      */
+    @Test
     public void testName() {
         Card[] testCards = new Card[84];
+        
+        testCards = CardFunctions.initDeck(testCards);
+        
         for (int i = 0; i < 84; i+=1) {
             assertTrue(testCards[i].getCardAttributeName().get_Name() == "1 Vor" ||
                        testCards[i].getCardAttributeName().get_Name() == "2 Vor" ||
