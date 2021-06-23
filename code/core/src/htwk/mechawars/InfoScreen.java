0ce2004922ab6c1905c63f19d62cbb7bd31017d0
tkretzsc <tim_michael.kretzschmar@stud.htwk-leipzig.de>
2021-06-23 17:59:28 +0200

    PopUp added (nothing to show now)

---
+++ code/core/src/htwk/mechawars/InfoScreen.java
@@ -0,0 +1,57 @@
+package htwk.mechawars;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Infoscreen, which includes Tipps and Tricks for the Game.
+ * Mostly 
+ */
+public class InfoScreen implements Screen{
+    
+    public InfoScreen() {
+        
+    }
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void render(float delta) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
