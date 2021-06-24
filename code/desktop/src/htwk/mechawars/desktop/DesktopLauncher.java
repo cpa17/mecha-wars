5c4e81ae9582ebe4fcb82c2bc657c6d4abdba769
cahlner <cahlner@stud.htwk-leipzig.de>
2021-06-24 16:59:15 +0200

    added code to DesktopLauncher

--- code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
+++ code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
@@ -7,6 +7,13 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class containing the runner for the desktop frontend.
  * Write .\gradlew.bat desktop:dist in your console, 
@@ -30,6 +37,9 @@ public class DesktopLauncher implements Runnable {
      * Main class, for the new CommandLine.
      */
     public static void main(String[] args) {
+        if (restartJVM()) {
+            return;
+        }
         System.exit(new CommandLine(new DesktopLauncher()).execute(args));
     }
 
@@ -42,6 +52,72 @@ public class DesktopLauncher implements Runnable {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setWindowedMode(1280, 720);
         new Lwjgl3Application(new MechaWars(), config);
+    }
+
+    /**
+     * Method, for fixing bug with macOS
+     */
+    public static boolean restartJVM() {
+
+        String osName = System.getProperty("os.name");
+
+        // if not a mac return false
+        if (!osName.startsWith("Mac") && !osName.startsWith("Darwin")) {
+            return false;
+        }
+
+        // get current jvm process pid
+        String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+        // get environment variable on whether XstartOnFirstThread is enabled
+        String env = System.getenv("JAVA_STARTED_ON_FIRST_THREAD_" + pid);
+
+        // if environment variable is "1" then XstartOnFirstThread is enabled
+        if (env != null && env.equals("1")) {
+            return false;
+        }
+
+        // restart jvm with -XstartOnFirstThread
+        String separator = System.getProperty("file.separator");
+        String classpath = System.getProperty("java.class.path");
+        String mainClass = System.getenv("JAVA_MAIN_CLASS_" + pid);
+        String jvmPath = System.getProperty("java.home") + separator + "bin" + separator + "java";
+
+        List<String> inputArguments = ManagementFactory.getRuntimeMXBean().getInputArguments();
+
+        ArrayList<String> jvmArgs = new ArrayList<String>();
+
+        jvmArgs.add(jvmPath);
+        jvmArgs.add("-XstartOnFirstThread");
+        jvmArgs.addAll(inputArguments);
+        jvmArgs.add("-cp");
+        jvmArgs.add(classpath);
+        jvmArgs.add(mainClass);
+
+        // if you don't need console output, just enable these two lines
+        // and delete bits after it. This JVM will then terminate.
+        //ProcessBuilder processBuilder = new ProcessBuilder(jvmArgs);
+        //processBuilder.start();
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(jvmArgs);
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            InputStream is = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
     }
 }
 
