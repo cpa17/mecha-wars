8d89ca038d5b8a39052170668e0818bb44fbf1c5
cahlner <cahlner@stud.htwk-leipzig.de>
2021-06-30 13:37:48 +0200

    added Robot test

---
+++ code/core/test/RobotTest.java
@@ -0,0 +1,63 @@
+import htwk.mechawars.board.Dir;
+import htwk.mechawars.board.Robot;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ *  Test for the class robot.
+ */
+
+public class RobotTest {
+    private Robot robot = new Robot();
+
+    @Test
+    public void robotTest() {
+        assertEquals(5, robot.getXcoor());
+        assertEquals(5, robot.getYcoor());
+        assertEquals(Dir.NORTH, robot.getDir());
+        assertEquals(0, robot.getStartX());
+        assertEquals(0, robot.getStartY());
+
+        robot.turn((byte) 3);
+
+        assertEquals(5, robot.getXcoor());
+        assertEquals(5, robot.getYcoor());
+        assertEquals(Dir.WEST, robot.getDir());
+        assertEquals(0, robot.getStartX());
+        assertEquals(0, robot.getStartY());
+
+        robot.moveInDirection((byte) 3);
+
+        assertEquals(2, robot.getXcoor());
+        assertEquals(5, robot.getYcoor());
+        assertEquals(Dir.WEST, robot.getDir());
+        assertEquals(0, robot.getStartX());
+        assertEquals(0, robot.getStartY());
+
+        robot.turn((byte) 2);
+        robot.moveInDirection((byte) 1);
+        robot.turn((byte) 1);
+        robot.moveInDirection((byte) -1);
+        robot.turn((byte) 2);
+        robot.moveInDirection((byte) 3);
+
+        assertEquals(3, robot.getXcoor());
+        assertEquals(1, robot.getYcoor());
+        assertEquals(Dir.NORTH, robot.getDir());
+        assertEquals(0, robot.getStartX());
+        assertEquals(0, robot.getStartY());
+
+    }
+
+    @BeforeEach
+    public void initRobot() {
+        robot.setDir(Dir.NORTH);
+        robot.setXcoor(5);
+        robot.setYcoor(5);
+        robot.setStartX(0);
+        robot.setStartY(0);
+    }
+
+}
