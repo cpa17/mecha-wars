bd834bc0bf4033fb38b8a60e500aec368abc0ffc
Uxa, Marc Andre <marc_andre.uxa@stud.htwk-leipzig.de>
2021-07-04 13:34:16 +0200

    styleguide fixes

--- code/core/src/htwk/mechawars/Player.java
+++ code/core/src/htwk/mechawars/Player.java
@@ -14,85 +14,100 @@ public class Player {
     private Texture life;
     private Texture damage;
     private Texture shutDown;
-    
+
     public Player() {
-        
+
     }
-    
+
     public void setDp() {
         damagePoints += 1;
     }
-    
+
     public void setLp() {
         lifePoints -= 1;
     }
-    
+
     public void setSd() {
         shutDownMark = true;
     }
-    
+
     public int getDp() {
         return damagePoints;
     }
-    
+
     public int getLp() {
         return lifePoints;
     }
-    
+
     public boolean getSd() {
         return shutDownMark;
-    }  
-    
+    }
+
     private void updateLife() {
-        switch(lifePoints) {
-            case 0: life = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 1: life = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 2: life = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 3: life = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            default: 
-                    break;
+        switch (lifePoints) {
+        case 0:
+            life = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 1:
+            life = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 2:
+            life = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 3:
+            life = new Texture(Gdx.files.internal("background.png"));
+            break;
+        default:
+            break;
         }
     }
-    
+
     private void updateDamage() {
-        switch(damagePoints) {
-            case 0: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 1: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 2: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 3: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 4: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 5: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 6: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 7: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 8: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            case 9: damage = new Texture(Gdx.files.internal("background.png"));
-                    break;
-            default:    
-                    break;
+        switch (damagePoints) {
+        case 0:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 1:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 2:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 3:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 4:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 5:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 6:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 7:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 8:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        case 9:
+            damage = new Texture(Gdx.files.internal("background.png"));
+            break;
+        default:
+            break;
         }
     }
-    
+
     private void updateShutDown() {
-        if(shutDownMark) 
+        if (shutDownMark) {
             shutDown = new Texture(Gdx.files.internal("background.png"));
-        else
+        }
+        else {
             shutDown = new Texture(Gdx.files.internal("background.png"));
+        }
     }
-    
-    
+
     void drawParameters(SpriteBatch batch) {
         updateLife();
         updateDamage();
