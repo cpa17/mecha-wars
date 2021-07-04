8d5aae370ee4a6d4fb55b14d0a704e9c213ba9d7
Uxa, Marc Andre <marc_andre.uxa@stud.htwk-leipzig.de>
2021-07-04 13:25:22 +0200

    advancing player class with update and draw functions for the parameters

--- code/core/src/htwk/mechawars/Player.java
+++ code/core/src/htwk/mechawars/Player.java
@@ -1,38 +1,104 @@
 package htwk.mechawars;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  * Class that represents the Player.
  */
 public class Player {
     private int lifePoints;
     private int damagePoints;
-    private boolean shutDown;
-  
+    private boolean shutDownMark;
+    private Texture life;
+    private Texture damage;
+    private Texture shutDown;
+    
     public Player() {
         
     }
     
-    void setDp() {
+    public void setDp() {
         damagePoints += 1;
     }
     
-    void setLp(int damage) {
-        lifePoints -= damage;
+    public void setLp() {
+        lifePoints -= 1;
     }
     
-    void setSd() {
-        shutDown = true;
+    public void setSd() {
+        shutDownMark = true;
     }
     
-    int getDp() {
+    public int getDp() {
         return damagePoints;
     }
     
-    int getLp() {
+    public int getLp() {
         return lifePoints;
     }
     
-    boolean getSd() {
-        return shutDown;
+    public boolean getSd() {
+        return shutDownMark;
     }  
+    
+    private void updateLife() {
+        switch(lifePoints) {
+            case 0: life = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 1: life = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 2: life = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 3: life = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            default: 
+                    break;
+        }
+    }
+    
+    private void updateDamage() {
+        switch(damagePoints) {
+            case 0: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 1: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 2: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 3: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 4: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 5: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 6: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 7: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 8: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            case 9: damage = new Texture(Gdx.files.internal("background.png"));
+                    break;
+            default:    
+                    break;
+        }
+    }
+    
+    private void updateShutDown() {
+        if(shutDownMark) 
+            shutDown = new Texture(Gdx.files.internal("background.png"));
+        else
+            shutDown = new Texture(Gdx.files.internal("background.png"));
+    }
+    
+    
+    void drawParameters(SpriteBatch batch) {
+        updateLife();
+        updateDamage();
+        updateShutDown();
+        batch.draw(life, 0, 0, 1280, 720);
+        batch.draw(damage, 0, 0, 1280, 720);
+        batch.draw(shutDown, 0, 0, 1280, 720);
+    }
 }
