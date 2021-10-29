a0431f32ed54d6fa85a34058a8e43d9f80ebc709
Uxa, Marc Andre <marc_andre.uxa@stud.htwk-leipzig.de>
2021-10-29 09:25:25 +0200

    pipeline fix

--- code/core/test/RobotTest.java
+++ code/core/test/RobotTest.java
@@ -19,8 +19,8 @@ public class RobotTest {
         assert robot.getbackupCopyX() >= 0;
         assert robot.getbackupCopyY() >= 0;
         
-        assert robot.getbackupCopyX() <= Gdx.graphics.getWidth();
-        assert robot.getbackupCopyY() <= Gdx.graphics.getHeight();
+        assert robot.getbackupCopyX() <= 720;
+        assert robot.getbackupCopyY() <= 1280;
 
         assertEquals(5, robot.getXcoor());
         assertEquals(5, robot.getYcoor());
