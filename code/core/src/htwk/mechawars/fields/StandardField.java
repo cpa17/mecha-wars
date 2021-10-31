96bd5bb122770a34bd01858433ea296c5b98c45d
Antonia Swiridoff <antonia.swiridoff@stud.htwk-leipzig.de>
2021-10-31 12:45:43 +0100

    Added attribute tile in StandardField to try to draw it that way

--- code/core/src/htwk/mechawars/fields/StandardField.java
+++ code/core/src/htwk/mechawars/fields/StandardField.java
@@ -1,15 +1,20 @@
 package htwk.mechawars.fields;
 
+import com.badlogic.gdx.graphics.Texture;
+
 /**
  * Class of a Standard Field.
  */
 public class StandardField extends Field {
+
+    private Texture tile;
 
     /**
      * Constructor of a Standard Field.
      */
     public StandardField(int xcoor, int ycoor) {
         super(xcoor, ycoor);
+        this.tile = new Texture("industrialTile.png");
     }
 
 }
