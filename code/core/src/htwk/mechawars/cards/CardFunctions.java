998f3cd94051eb745b94a3ab8d5aacca10923576
tkretzsc <tim_michael.kretzschmar@stud.htwk-leipzig.de>
2021-11-12 21:59:57 +0100

    Changes to ArrayList, but Test all wrong. Must be checked soon

--- code/core/src/htwk/mechawars/cards/CardFunctions.java
+++ code/core/src/htwk/mechawars/cards/CardFunctions.java
@@ -1,9 +1,7 @@
 package htwk.mechawars.cards;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 /**
  * Includes the functions, that are needed for working with an cardDeck.
@@ -19,17 +17,55 @@ public class CardFunctions {
      *
      * @return The initial cardDeck
      */
-    public static Card[] initDeck() {
-        Card[] cardDeck = new Card[84];
+    public static ArrayList<Card> initDeck(ArrayList<Card> cardDeck) {
+        
+        cardDeck.clear();   // make deck empty to be sure the List can be initialiced
+                
+        //Card[] cardDeck = new Card[84];
 
         int x = 10;
         // List<Card> deck = new ArrayList<>(Arrays.asList(cardDeck));
         // First 6 cards (U-Turn)
         for (int i = 0; i < 6; i += 1) {
-            //MUSTER : deck.add(new Card(Type.turn, (byte) 2, x));
-            cardDeck[i]      = new Card(Type.turn, (byte) 2, x);
+            cardDeck.add(new Card(Type.turn, (byte) 2, x));
+            //cardDeck[i]      = new Card(Type.turn, (byte) 2, x);
             x += 10;
         }
+        
+        // Left-Turn & Right-Turn
+        for (int i = 6; i < 42; i += 1) {
+            cardDeck.add(new Card(Type.turn, (byte) 3, x));
+            x += 10;
+            cardDeck.add(new Card(Type.turn, (byte) 1, x));
+            x += 10;
+        }
+        
+        x = 430;
+        
+        for (int i = 42; i < 48; i += 1) {
+            cardDeck.add(new Card(Type.mov, (byte) -1, x));
+            x += 10;
+        }
+        // x = 490
+        // Card 49 .. 66
+        for (int i = 48; i < 66; i += 1) {
+            cardDeck.add(new Card(Type.mov, (byte) 1, x));
+            x += 10;
+        }
+        // x = 670
+        // Card 67 .. 78
+        for (int i = 66; i < 78; i += 1) {
+            cardDeck.add(new Card(Type.mov, (byte) 2, x));
+            x += 10;
+        }
+        // x = 490
+        // Card 79 .. 84
+        for (int i = 78; i < 84; i += 1) {
+            cardDeck.add(new Card(Type.mov, (byte) 3, x));
+            x += 10;
+        }
+        
+        /*
         // x = 70;
         // Card 7, 9, 11 .. 41 (Left-Turn)
         for (int i = 6; i < 41; i += 2) {
@@ -68,6 +104,7 @@ public class CardFunctions {
             cardDeck[i]      = new Card(Type.mov, (byte) 3, x);
             x += 10;
         }
+        */
 
         //-------------------------------------------------------------
         /*
@@ -86,22 +123,20 @@ public class CardFunctions {
         }
         */
         //-------------------------------------------------------------
+        
         return cardDeck;
     }
 
     /**
      * Shuffles the cards, which were created before.
      *
-     * @param originalDeck -> includes the cards to shuffle
-     * @return Array of cards, that are shuffled
+     * @param cardDeck -> includes the cards to shuffle
+     * @return List of cards, that are shuffled
      */
+    public static ArrayList<Card> shuffle(ArrayList<Card> cardDeck) {
+        // Function from Java to shuffle the List
+        Collections.shuffle(cardDeck);
 
-    public static Card[] shuffle(Card[] originalDeck) {
-        List<Card> deck = new ArrayList<>(Arrays.asList(originalDeck));
-
-        Collections.shuffle(deck);
-        List<Card> shuffled = new ArrayList<>(deck);
-
-        return shuffled.toArray(new Card[shuffled.size()]);
+        return cardDeck;
     }
 }
