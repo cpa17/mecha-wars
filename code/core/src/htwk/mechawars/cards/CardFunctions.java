75be7ba8557766cf09dd2ad1dfb7173388a388be
kblechsc <konstantin.blechschmidt@stud.htwk-leipzig.de>
2021-11-13 15:05:09 +0100

    Tests for changed deckstructure now working. (Arraylist had to be initialized)

--- code/core/src/htwk/mechawars/cards/CardFunctions.java
+++ code/core/src/htwk/mechawars/cards/CardFunctions.java
@@ -19,110 +19,47 @@ public class CardFunctions {
      */
     public static ArrayList<Card> initDeck(ArrayList<Card> cardDeck) {
         
-        cardDeck.clear();   // make deck empty to be sure the List can be initialiced
-                
-        //Card[] cardDeck = new Card[84];
+        cardDeck.clear();   //clear Arraylist completely for initialization
 
-        int x = 10;
-        // List<Card> deck = new ArrayList<>(Arrays.asList(cardDeck));
-        // First 6 cards (U-Turn)
+        int priority = 10;
+
+        // 6 Cards: U-Turn / priority 10-60
         for (int i = 0; i < 6; i += 1) {
-            cardDeck.add(new Card(Type.turn, (byte) 2, x));
-            //cardDeck[i]      = new Card(Type.turn, (byte) 2, x);
-            x += 10;
+            cardDeck.add(new Card(Type.turn, (byte) 2, priority));
+            priority += 10;
         }
         
-        // Left-Turn & Right-Turn
-        for (int i = 6; i < 42; i += 1) {
-            cardDeck.add(new Card(Type.turn, (byte) 3, x));
-            x += 10;
-            cardDeck.add(new Card(Type.turn, (byte) 1, x));
-            x += 10;
-        }
-        
-        x = 430;
-        
-        for (int i = 42; i < 48; i += 1) {
-            cardDeck.add(new Card(Type.mov, (byte) -1, x));
-            x += 10;
-        }
-        // x = 490
-        // Card 49 .. 66
-        for (int i = 48; i < 66; i += 1) {
-            cardDeck.add(new Card(Type.mov, (byte) 1, x));
-            x += 10;
-        }
-        // x = 670
-        // Card 67 .. 78
-        for (int i = 66; i < 78; i += 1) {
-            cardDeck.add(new Card(Type.mov, (byte) 2, x));
-            x += 10;
-        }
-        // x = 490
-        // Card 79 .. 84
-        for (int i = 78; i < 84; i += 1) {
-            cardDeck.add(new Card(Type.mov, (byte) 3, x));
-            x += 10;
-        }
-        
-        /*
-        // x = 70;
-        // Card 7, 9, 11 .. 41 (Left-Turn)
-        for (int i = 6; i < 41; i += 2) {
-            cardDeck[i]      = new Card(Type.turn, (byte) 3, x);
-            x += 20;
-        }
-
-        x = 80;
-        // Card 8, 10 .. 42 (Right-Turn)
-        for (int i = 7; i < 42; i += 2) {
-            cardDeck[i]      = new Card(Type.turn, (byte) 1, x);
-            x += 20;
-        }
-
-        x = 430;
-        // Card 43 .. 48
-        for (int i = 42; i < 48; i += 1) {
-            cardDeck[i]      = new Card(Type.mov, (byte) -1, x);
-            x += 10;
-        }
-        // x = 490
-        // Card 49 .. 66
-        for (int i = 48; i < 66; i += 1) {
-            cardDeck[i]      = new Card(Type.mov, (byte) 1, x);
-            x += 10;
-        }
-        // x = 670
-        // Card 67 .. 78
-        for (int i = 66; i < 78; i += 1) {
-            cardDeck[i]      = new Card(Type.mov, (byte) 2, x);
-            x += 10;
-        }
-        // x = 490
-        // Card 79 .. 84
-        for (int i = 78; i < 84; i += 1) {
-            cardDeck[i]      = new Card(Type.mov, (byte) 3, x);
-            x += 10;
-        }
-        */
-
-        //-------------------------------------------------------------
-        /*
+        // 18 Cards: Left-Turn & Right-Turn / priority 70-420
         for (int i = 0; i < 18; i += 1) {
-            cardDeck[i]      = new Card(Type.mov, (byte) 1, (byte) 0);
-            cardDeck[i + 18] = new Card(Type.turn, (byte) 1, (byte) 0);
-            cardDeck[i + 36] = new Card(Type.turn, (byte) 3, (byte) 0);
+            cardDeck.add(new Card(Type.turn, (byte) 3, priority));
+            priority += 10;
+            cardDeck.add(new Card(Type.turn, (byte) 1, priority));
+            priority += 10;
         }
-        for (int i = 54; i < 60; i += 1) {
-            cardDeck[i]      = new Card(Type.mov, (byte) 3, (byte) 0);
-            cardDeck[i + 6]  = new Card(Type.mov, (byte) -1, (byte) 0);
-            cardDeck[i + 12] = new Card(Type.turn, (byte) 2, (byte) 0);
+
+        // 6 Cards: Backup / priority 430-480
+        for (int i = 0; i < 6; i += 1) {
+            cardDeck.add(new Card(Type.mov, (byte) -1, priority));
+            priority += 10;
         }
-        for (int i = 72; i < 84; i += 1) {
-            cardDeck[i]      = new Card(Type.mov, (byte) 2, (byte) 0);
+
+        // 18 Cards: Move 1 / priority 490-660
+        for (int i = 0; i < 18; i += 1) {
+            cardDeck.add(new Card(Type.mov, (byte) 1, priority));
+            priority += 10;
         }
-        */
-        //-------------------------------------------------------------
+
+        // 12 Cards: Move 2 / priority 670-780
+        for (int i = 0; i < 12; i += 1) {
+            cardDeck.add(new Card(Type.mov, (byte) 2, priority));
+            priority += 10;
+        }
+
+        // 6 Cards: Move 3 / priority 790-840
+        for (int i = 0; i < 6; i += 1) {
+            cardDeck.add(new Card(Type.mov, (byte) 3, priority));
+            priority += 10;
+        }
         
         return cardDeck;
     }
