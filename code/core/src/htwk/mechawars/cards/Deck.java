446a70b32f942de9d7a141d691986d83c5fc6fc8
tkretzsc <tim_michael.kretzschmar@stud.htwk-leipzig.de>
2021-11-15 15:27:07 +0100

    shuffle and initDeck edited to be smarter ;)

--- code/core/src/htwk/mechawars/cards/Deck.java
+++ code/core/src/htwk/mechawars/cards/Deck.java
@@ -14,11 +14,11 @@ public class Deck {
      * Constructor of a complete Deck.
      */
     public Deck() {
-        initDeck(deck);
+        initDeck();
     }
 
     /**
-     * Getter-Function for the ArrayList<Card> deck.
+     * Getter-Function for the ArrayList<\Card\> deck.
      * 
      * @return the hole deck (which is an ArrayList of Cards) 
      */
@@ -34,63 +34,57 @@ public class Deck {
      *
      * @return The initial cardDeck
      */
-    public ArrayList<Card> initDeck(ArrayList<Card> cardDeck) {
+    public void initDeck() {
 
-        cardDeck.clear();   //clear Arraylist completely for initialization
+        deck.clear();   //clear Arraylist completely for initialization
 
         int priority = 10;
 
         // 6 Cards: U-Turn / priority 10-60
         for (int i = 0; i < 6; i += 1) {
-            cardDeck.add(new Card(Type.turn, (byte) 2, priority));
+            deck.add(new Card(Type.turn, (byte) 2, priority));
             priority += 10;
         }
 
         // 18 Cards: Left-Turn & Right-Turn / priority 70-420
         for (int i = 0; i < 18; i += 1) {
-            cardDeck.add(new Card(Type.turn, (byte) 3, priority));
+            deck.add(new Card(Type.turn, (byte) 3, priority));
             priority += 10;
-            cardDeck.add(new Card(Type.turn, (byte) 1, priority));
+            deck.add(new Card(Type.turn, (byte) 1, priority));
             priority += 10;
         }
 
         // 6 Cards: Backup / priority 430-480
         for (int i = 0; i < 6; i += 1) {
-            cardDeck.add(new Card(Type.mov, (byte) -1, priority));
+            deck.add(new Card(Type.mov, (byte) -1, priority));
             priority += 10;
         }
 
         // 18 Cards: Move 1 / priority 490-660
         for (int i = 0; i < 18; i += 1) {
-            cardDeck.add(new Card(Type.mov, (byte) 1, priority));
+            deck.add(new Card(Type.mov, (byte) 1, priority));
             priority += 10;
         }
 
         // 12 Cards: Move 2 / priority 670-780
         for (int i = 0; i < 12; i += 1) {
-            cardDeck.add(new Card(Type.mov, (byte) 2, priority));
+            deck.add(new Card(Type.mov, (byte) 2, priority));
             priority += 10;
         }
 
         // 6 Cards: Move 3 / priority 790-840
         for (int i = 0; i < 6; i += 1) {
-            cardDeck.add(new Card(Type.mov, (byte) 3, priority));
+            deck.add(new Card(Type.mov, (byte) 3, priority));
             priority += 10;
         }
-
-        return cardDeck;
     }
 
     /**
      * Shuffles the cards, which were created before.
-     *
-     * @param cardDeck -> includes the cards to shuffle
-     * @return List of cards, that are shuffled
      */
-    public ArrayList<Card> shuffle(ArrayList<Card> cardDeck) {
+    public void shuffle() {
         // Function from Java to shuffle the List
-        Collections.shuffle(cardDeck);
+        Collections.shuffle(deck);
 
-        return cardDeck;
     }
 }
