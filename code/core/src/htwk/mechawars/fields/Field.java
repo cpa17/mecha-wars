19b7bb25ba264e40fb97afd49844176c8d41ca78
tkretzsc <tim_michael.kretzschmar@stud.htwk-leipzig.de>
2021-11-19 16:39:56 +0100

    commentary fixed (google)?

--- code/core/src/htwk/mechawars/fields/Field.java
+++ code/core/src/htwk/mechawars/fields/Field.java
@@ -43,15 +43,22 @@ public class Field {
         return this.ycoor;
     }
 
+    /**
+     * Dummy.
+     * 
+     * @return .
+     */
     public Texture getTile() {
         return this.tile;
     }
     
-    public Robot action(Robot robot) {
-        
-        // On the field -> Repair
-        robot.onRepairField();
-        
+    /**
+     * On the Field, a robot action can be done.
+     * 
+     * @param robot
+     * @return the instance robot
+     */
+    public Robot action(Robot robot) {        
         return robot; 
     }
 }
