2d191c139caa777ea006b0d38930a53d7e098106
tkretzsc <tim_michael.kretzschmar@stud.htwk-leipzig.de>
2021-11-28 16:11:44 +0100

    export function added

--- code/core/src/htwk/mechawars/fieldeditor/ButtonfunctionsFieldEditor.java
+++ code/core/src/htwk/mechawars/fieldeditor/ButtonfunctionsFieldEditor.java
@@ -3,11 +3,16 @@ package htwk.mechawars.fieldeditor;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class ButtonfunctionsFieldEditor {
 
@@ -61,7 +66,54 @@ public class ButtonfunctionsFieldEditor {
     }
 
     private void exportField() {
+        
+        JFrame Speicherfenster = new JFrame();           
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Datei speichern unter...");  
 
+        File filepfadLinkListe = new File("code\\desktop\\bin\\main");
+        fileChooser.setCurrentDirectory(filepfadLinkListe);
+
+        int userSelection = fileChooser.showSaveDialog(Speicherfenster);
+
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            if(!fileToSave.getAbsolutePath().endsWith(".txt")) {
+                fileToSave = new File(fileToSave.getAbsoluteFile() + ".txt");
+            }
+            save(fileToSave);
+        }
+
+    }
+    
+    /**
+     * 
+     * @param file
+     * @param liste
+     */
+    public boolean save(File file) {
+        
+        OutputStream ostream;
+        try {
+            ostream = new FileOutputStream(file);
+            PrintWriter schreiber = new PrintWriter(ostream);
+
+            for (int index = 0; index<actuallField.size(); index += 1) {
+                for (int row = 0; row < 12; row += 1) {
+                    schreiber.print(actuallField.get(index) + " ");
+                }
+                schreiber.println();
+            }
+
+            schreiber.close();
+            
+            return true;
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null,"Fehler!");
+            e.printStackTrace();
+            return false;
+        }
+        
     }
 
     private void resetField() {
