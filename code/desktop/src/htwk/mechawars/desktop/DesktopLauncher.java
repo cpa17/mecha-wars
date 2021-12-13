7dc57c4c5c24493a0eb6174a548bfe72d3b3cb40
cpa17 <82649637+cpa17@users.noreply.github.com>
2021-12-13 15:03:07 +0100

    added Vault Assault

--- code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
+++ code/desktop/src/htwk/mechawars/desktop/DesktopLauncher.java
@@ -36,8 +36,8 @@ public class DesktopLauncher implements Runnable {
     boolean skip;
 
     @Option(names = { "-b", "--board" },
-            description = "Choose a Gameboard (ChopChopShopChallenge.txt, map2.txt)")
-    private String fileName = "ChopShopChallenge.txt";
+            description = "Choose a Gameboard (ChopChopShopChallenge.txt, VaultAssault.txt)")
+    private String fileName = "VaultAssault.txt";
 
     @Option(names = { "-p", "--player" }, description = "Number of Players")
     int player = 4;
