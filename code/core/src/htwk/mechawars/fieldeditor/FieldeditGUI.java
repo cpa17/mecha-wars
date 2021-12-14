cae5e31f7da44b6f3328ba725bb99aa20543764b
kblechsc <konstantin.blechschmidt@stud.htwk-leipzig.de>
2021-12-14 14:18:23 +0100

    info on hover + missing buttons added

--- code/core/src/htwk/mechawars/fieldeditor/FieldeditGUI.java
+++ code/core/src/htwk/mechawars/fieldeditor/FieldeditGUI.java
@@ -15,8 +15,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.TextTooltip;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -53,6 +55,7 @@ public class FieldeditGUI implements Screen{
     private int typeLaser = 0; //type of LaserButton
     private int repairNum = 2; //number of RepairButton
     private int startFieldNumber = 1; //number of StartFieldButton
+    private int pusherNumber = 1; //Number of Pusher
     private boolean backToMain = false;
     
     public FieldeditGUI(FieldEditor fieldEditor, String map) {
@@ -110,223 +113,165 @@ public class FieldeditGUI implements Screen{
             }
         });
         
-
+        TextField textFieldInfo = new TextField(":)", skinFiEdit);
+        textFieldInfo.setAlignment(Align.center);
+        textFieldInfo.setSize(buttonWidth + 100, buttonHeight);
+        textFieldInfo.setPosition(funcButtonPosX + 40, funcButtonPosY - 610);
+        
         ImageButton barrierCornerButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/" + "barriercorner/"
                         + "BarrierCorner0" + String.valueOf(corner) + ".png"))));
         barrierCornerButton.setSize(picButtonSize, picButtonSize);
         barrierCornerButton.setPosition(picButtonPosX, picButtonPosY);
-//        barrierCornerButton.addListener(new ClickListener() {
-//            public void enter() {
-//                
-//            }
-//            public void exit() {
-//                
-//            }
-//            public void clicked(InputEvent event, float x, float y) {
-////                drawOnField(x, y, corner);
-//            }
-//        });
+        barrierCornerButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+//                drawOnField(x, y, corner);
+            }
+        });
+        barrierCornerButton.addListener(new TextTooltip("Barrier Corner", skinFiEdit));
+        
         ImageButton barrierSideButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/" + "barrierside/"
                         + "BarrierSide" + String.valueOf(side) + ".png"))));
         barrierSideButton.setSize(picButtonSize, picButtonSize);
         barrierSideButton.setPosition(picButtonPosX + 90, picButtonPosY);
-//      barrierSideButton.addListener(new ClickListener() {
-//          public void enter() {
-//                
-//          }
-//          public void exit() {
-//                
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        barrierSideButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y, side);
-//          }
-//      });
+          }
+        });
+        barrierSideButton.addListener(new TextTooltip("Barrier Side", skinFiEdit));
+        
         ImageButton blackHoleButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/BlackHole.png"))));
         blackHoleButton.setSize(picButtonSize, picButtonSize);
         blackHoleButton.setPosition(picButtonPosX + 180, picButtonPosY);
-//      blackHoleButton.addListener(new ClickListener() {
-//          public void enter() {
-//      
-//          }
-//          public void exit() {
-//      
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        blackHoleButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y);
-//          }
-//      });
-        
-        
-//        ImageButton blockadeButton = new ImageButton(new TextureRegionDrawable(
-//                new TextureRegion(new Texture("mapAssets/" + "blockade/"
-//                        + "Blockade0" + String.valueOf(type) + ".png"))));
-//      blockadeButton.setSize(buttonWidth, buttonHeight);
-//      blockadeButton.setPosition(x, y);
-//      blockadeButton.addListener(new ClickListener() {
-//          public void enter() {
-//  
-//          }
-//          public void exit() {
-//  
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
-//              drawOnField(x, y, type);
-//          }
-//      });
-        
+          }
+        });
+        blackHoleButton.addListener(new TextTooltip("Black Hole", skinFiEdit));
         
         ImageButton checkpointButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/checkpoints/Check" 
                         + checkpointNumber + ".png"))));
         checkpointButton.setSize(picButtonSize, picButtonSize);
         checkpointButton.setPosition(picButtonPosX, picButtonPosY - 90);
-//      checkpointButton.addListener(new ClickListener() {
-//          public void enter() {
-//
-//          }
-//          public void exit() {
-//
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        checkpointButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y, checkpointNumber);
-//          }
-//      });
+          }
+        });
+        checkpointButton.addListener(new TextTooltip("Checkpoint", skinFiEdit));
+        
         ImageButton conveyorBeltButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/" + "conveyorBelt/"
                         + "ConveyorBelt" + String.valueOf(startConveyor) 
                         + String.valueOf(endConveyor) + ".png"))));
         conveyorBeltButton.setSize(picButtonSize, picButtonSize);
         conveyorBeltButton.setPosition(picButtonPosX + 90, picButtonPosY - 90);
-//      conveyorBeltButton.addListener(new ClickListener() {
-//          public void enter() {
-//
-//          }
-//          public void exit() {
-//
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        conveyorBeltButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y, endConveyor);
-//          }
-//      });
+          }
+        });
+        conveyorBeltButton.addListener(new TextTooltip("Conveyorbelt", skinFiEdit));
+        
         ImageButton expressConveyorBeltButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/" + "expressconveyorBelt/"
                         + "ExpressConveyorBelt" + String.valueOf(startExpressConveyor) 
                         + String.valueOf(endExpressConveyor) + ".png"))));
         expressConveyorBeltButton.setSize(picButtonSize, picButtonSize);
         expressConveyorBeltButton.setPosition(picButtonPosX + 180, picButtonPosY - 90);
-//      expressConveyorBeltButton.addListener(new ClickListener() {
-//          public void enter() {
-//
-//          }
-//          public void exit() {
-//
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        expressConveyorBeltButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y, endExpressConveyor);
-//          }
-//      });
-        ImageButton fieldButton = new ImageButton(new TextureRegionDrawable(
-                new TextureRegion(new Texture("mapAssets/StandardField.png"))));
-        fieldButton.setSize(picButtonSize, picButtonSize);
-        fieldButton.setPosition(picButtonPosX, picButtonPosY - 180);
-//      fieldButton.addListener(new ClickListener() {
-//          public void enter() {
-//
-//          }
-//          public void exit() {
-//
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+          }
+        });
+        expressConveyorBeltButton.addListener(new TextTooltip("Express Conveyorbelt", skinFiEdit));
+        
+        ImageButton backupButton = new ImageButton(new TextureRegionDrawable(
+                new TextureRegion(new Texture("mapAssets/Checkpoint.png"))));
+        backupButton.setSize(picButtonSize, picButtonSize);
+        backupButton.setPosition(picButtonPosX, picButtonPosY - 180);
+        backupButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y);
-//          }
-//      });
+          }
+        });
+        backupButton.addListener(new TextTooltip("Backup Checkpoint", skinFiEdit));
+        
         ImageButton gearButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/gear/Gear0" + gearNumber + ".png"))));
         gearButton.setSize(picButtonSize, picButtonSize);
         gearButton.setPosition(picButtonPosX + 90, picButtonPosY - 180);
-//      gearButton.addListener(new ClickListener() {
-//          public void enter() {
-//
-//          }
-//          public void exit() {
-//
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        gearButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y, gearNumber);
-//          }
-//      });
+          }
+        });
+        gearButton.addListener(new TextTooltip("Gear", skinFiEdit));
+        
         ImageButton laserButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/" + "laser/"
                         + "Laser0" + String.valueOf(typeLaser) + ".png"))));
         laserButton.setSize(picButtonSize, picButtonSize);
         laserButton.setPosition(picButtonPosX + 180, picButtonPosY - 180);
-//      laserButton.addListener(new ClickListener() {
-//          public void enter() {
-//
-//          }
-//          public void exit() {
-//
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        laserButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y, typeLaser);
-//          }
-//      });
+          }
+        });
+        laserButton.addListener(new TextTooltip("Laser", skinFiEdit));
+        
         ImageButton repairSiteButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/repairsite/RepairSite0" + repairNum
                         + ".png"))));
         repairSiteButton.setSize(picButtonSize, picButtonSize);
         repairSiteButton.setPosition(picButtonPosX, picButtonPosY - 270);
-//      repairSiteButton.addListener(new ClickListener() {
-//          public void enter() {
-//
-//          }
-//          public void exit() {
-//
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        repairSiteButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y, repairNum);
-//          }
-//      });
+          }
+        });
+        repairSiteButton.addListener(new TextTooltip("Repair Site", skinFiEdit));
+        
         ImageButton standardFieldButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/StandardField.png"))));
         standardFieldButton.setSize(picButtonSize, picButtonSize);
         standardFieldButton.setPosition(picButtonPosX + 90, picButtonPosY - 270);
-//      standardFieldButton.addListener(new ClickListener() {
-//          public void enter() {
-//
-//          }
-//          public void exit() {
-//
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        standardFieldButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y);
-//          }
-//      });
+          }
+        });
+        standardFieldButton.addListener(new TextTooltip("Standard Feld", skinFiEdit));
+        
         ImageButton startFieldButton = new ImageButton(new TextureRegionDrawable(
                 new TextureRegion(new Texture("mapAssets/" + "startfield/"
                         + "StartField0" + String.valueOf(startFieldNumber) + ".png"))));
         startFieldButton.setSize(picButtonSize, picButtonSize);
         startFieldButton.setPosition(picButtonPosX + 180, picButtonPosY - 270);
-//      startFieldButton.addListener(new ClickListener() {
-//          public void enter() {
-//
-//          }
-//          public void exit() {
-//
-//          }
-//          public void clicked(InputEvent event, float x, float y) {
+        startFieldButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
 //              drawOnField(x, y, startFieldNumber);
-//          }
-//      });
-        
-        TextField textFieldInfo = new TextField(":)", skinFiEdit);
-        textFieldInfo.setAlignment(Align.center);
-        textFieldInfo.setSize(buttonWidth + 100, buttonHeight);
-        textFieldInfo.setPosition(funcButtonPosX + 40, funcButtonPosY - 520);
+          }
+        });
+        startFieldButton.addListener(new TextTooltip("Start Feld", skinFiEdit));
 
+        ImageButton pusherButton = new ImageButton(new TextureRegionDrawable(
+                new TextureRegion(new Texture("mapAssets/" + "pusher/"
+                        + "Pusher0" + String.valueOf(pusherNumber) + ".png"))));
+        pusherButton.setSize(picButtonSize, picButtonSize);
+        pusherButton.setPosition(picButtonPosX + 90, picButtonPosY - 360);
+        startFieldButton.addListener(new ClickListener() {
+          public void clicked(InputEvent event, float x, float y) {
+//              drawOnField(x, y, startFieldNumber);
+          }
+        });
+        pusherButton.addListener(new TextTooltip("Pusher", skinFiEdit));
+        
         stageFiEdit.addActor(importButton);
         stageFiEdit.addActor(exportButton);
         stageFiEdit.addActor(backButton);
@@ -337,12 +282,13 @@ public class FieldeditGUI implements Screen{
         stageFiEdit.addActor(checkpointButton);
         stageFiEdit.addActor(conveyorBeltButton);
         stageFiEdit.addActor(expressConveyorBeltButton);
-        stageFiEdit.addActor(fieldButton);
+        stageFiEdit.addActor(backupButton);
         stageFiEdit.addActor(gearButton);
         stageFiEdit.addActor(laserButton);
         stageFiEdit.addActor(repairSiteButton);
         stageFiEdit.addActor(standardFieldButton);
         stageFiEdit.addActor(startFieldButton);
+        stageFiEdit.addActor(pusherButton);
         stageFiEdit.addActor(textFieldInfo);
         
     }
