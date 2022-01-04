c6637dca097fb74bfa60d029123787c810d36e6e
kblechsc <konstantin.blechschmidt@stud.htwk-leipzig.de>
2022-01-04 18:44:33 +0100

    added cases

--- code/core/src/htwk/mechawars/fieldeditor/BoardToInt.java
+++ code/core/src/htwk/mechawars/fieldeditor/BoardToInt.java
@@ -69,153 +69,153 @@ public class BoardToInt {
                 case "Check8" : list.add(10408);
                     break;
                     
-                case "ConveyorBelt02" :
+                case "ConveyorBelt02" : list.add(10502);
                     break;
-                case "ConveyorBelt03" :
+                case "ConveyorBelt03" : list.add(10503);
                     break;
-                case "ConveyorBelt12" :
+                case "ConveyorBelt12" : list.add(10512);
                     break;
-                case "ConveyorBelt13" :
+                case "ConveyorBelt13" : list.add(10513);
                     break;
-                case "ConveyorBelt14" :
+                case "ConveyorBelt14" : list.add(10514);
                     break;
-                case "ConveyorBelt21" :
+                case "ConveyorBelt21" : list.add(10521);
                     break;
-                case "ConveyorBelt23" :
+                case "ConveyorBelt23" : list.add(10523);
                     break;
-                case "ConveyorBelt24" :
+                case "ConveyorBelt24" : list.add(10524);
                     break;
-                case "ConveyorBelt31" :
+                case "ConveyorBelt31" : list.add(10531);
                     break;
-                case "ConveyorBelt32" :
+                case "ConveyorBelt32" : list.add(10532);
                     break;
-                case "ConveyorBelt34" :
+                case "ConveyorBelt34" : list.add(10534);
                     break;
-                case "ConveyorBelt41" :
+                case "ConveyorBelt41" : list.add(10541);
                     break;
-                case "ConveyorBelt42" :
+                case "ConveyorBelt42" : list.add(10542);
                     break;
-                case "ConveyorBelt43" :
+                case "ConveyorBelt43" : list.add(10543);
                     break;
-                case "ConveyorBelt52" :
+                case "ConveyorBelt52" : list.add(10552);
                     break;
-                case "ConveyorBelt54" :
+                case "ConveyorBelt54" : list.add(10554);
                     break;
-                case "ConveyorBelt61" :
+                case "ConveyorBelt61" : list.add(10561);
                     break;
-                case "ConveyorBelt63" :
+                case "ConveyorBelt63" : list.add(10563);
                     break;
-                case "ConveyorBelt71" :
+                case "ConveyorBelt71" : list.add(10571);
                     break;
-                case "ConveyorBelt74" :
+                case "ConveyorBelt74" : list.add(10574);
                     break;
-                case "ConveyorBelt83" :
+                case "ConveyorBelt83" : list.add(10583);
                     break;
-                case "ConveyorBelt84" :
+                case "ConveyorBelt84" : list.add(10584);
                     break;
-                case "ConveyorBelt91" :
+                case "ConveyorBelt91" : list.add(10591);
                     break;
-                case "ConveyorBelt92" :
+                case "ConveyorBelt92" : list.add(10592);
                     break;
-                    
-                case "ExpressConveyorBelt02" :
+
+                case "ExpressConveyorBelt02" : list.add(10602);
                     break;
-                case "ExpressConveyorBelt03" :
+                case "ExpressConveyorBelt03" : list.add(10603);
                     break;
-                case "ExpressConveyorBelt12" :
+                case "ExpressConveyorBelt12" : list.add(10612);
                     break;
-                case "ExpressConveyorBelt13" :
+                case "ExpressConveyorBelt13" : list.add(10613);
                     break;
-                case "ExpressConveyorBelt14" :
+                case "ExpressConveyorBelt14" : list.add(10614);
                     break;
-                case "ExpressConveyorBelt21" :
+                case "ExpressConveyorBelt21" : list.add(10621);
                     break;
-                case "ExpressConveyorBelt23" :
+                case "ExpressConveyorBelt23" : list.add(10623);
                     break;
-                case "ExpressConveyorBelt24" :
+                case "ExpressConveyorBelt24" : list.add(10624);
                     break;
-                case "ExpressConveyorBelt31" :
+                case "ExpressConveyorBelt31" : list.add(10631);
                     break;
-                case "ExpressConveyorBelt32" :
+                case "ExpressConveyorBelt32" : list.add(10632);
                     break;
-                case "ExpressConveyorBelt34" :
+                case "ExpressConveyorBelt34" : list.add(10634);
                     break;
-                case "ExpressConveyorBelt41" :
+                case "ExpressConveyorBelt41" : list.add(10641);
                     break;
-                case "ExpressConveyorBelt42" :
+                case "ExpressConveyorBelt42" : list.add(10642);
                     break;
-                case "ExpressConveyorBelt43" :
+                case "ExpressConveyorBelt43" : list.add(10643);
                     break;
-                case "ExpressConveyorBelt52" :
+                case "ExpressConveyorBelt52" : list.add(10652);
                     break;
-                case "ExpressConveyorBelt54" :
+                case "ExpressConveyorBelt54" : list.add(10654);
                     break;
-                case "ExpressConveyorBelt61" :
+                case "ExpressConveyorBelt61" : list.add(10661);
                     break;
-                case "ExpressConveyorBelt63" :
+                case "ExpressConveyorBelt63" : list.add(10663);
                     break;
-                case "ExpressConveyorBelt71" :
+                case "ExpressConveyorBelt71" : list.add(10671);
                     break;
-                case "ExpressConveyorBelt74" :
+                case "ExpressConveyorBelt74" : list.add(10674);
                     break;
-                case "ExpressConveyorBelt83" :
+                case "ExpressConveyorBelt83" : list.add(10683);
                     break;
-                case "ExpressConveyorBelt84" :
+                case "ExpressConveyorBelt84" : list.add(10684);
                     break;
-                case "ExpressConveyorBelt91" :
+                case "ExpressConveyorBelt91" : list.add(10691);
                     break;
-                case "ExpressConveyorBelt92" :
-                    break;
-                    
-                case "Gear01" :
-                    break;
-                case "Gear02" :
+                case "ExpressConveyorBelt92" : list.add(10692);
                     break;
                     
-                case "Laser00" :
+                case "Gear01" : list.add(10701);
                     break;
-                case "Laser01" :
-                    break;
-                case "Laser02" :
-                    break;
-                case "Laser03" :
-                    break;
-                case "Laser04" :
-                    break;
-                case "Laser05" :
-                    break;
-                case "Laser06" :
-                    break;
-                case "Laser07" :
-                    break;
-                case "Laser08" :
-                    break;
-                case "Laser09" :
+                case "Gear02" : list.add(10702);
                     break;
                     
-                case "StartField01" :
+                case "Laser00" : list.add(10800);
                     break;
-                case "StartField02" :
+                case "Laser01" : list.add(10801);
                     break;
-                case "StartField03" :
+                case "Laser02" : list.add(10802);
                     break;
-                case "StartField04" :
+                case "Laser03" : list.add(10803);
                     break;
-                case "StartField05" :
+                case "Laser04" : list.add(10804);
                     break;
-                case "StartField06" :
+                case "Laser05" : list.add(10805);
                     break;
-                case "StartField07" :
+                case "Laser06" : list.add(10806);
                     break;
-                case "StartField08" :
+                case "Laser07" : list.add(10807);
+                    break;
+                case "Laser08" : list.add(10808);
+                    break;
+                case "Laser09" : list.add(10809);
                     break;
                     
-                case "RepairSite01" :
+                case "StartField01" : list.add(11101);
                     break;
-                case "RepairSite02" :
+                case "StartField02" : list.add(11102);
+                    break;
+                case "StartField03" : list.add(11103);
+                    break;
+                case "StartField04" : list.add(11104);
+                    break;
+                case "StartField05" : list.add(11105);
+                    break;
+                case "StartField06" : list.add(11106);
+                    break;
+                case "StartField07" : list.add(11107);
+                    break;
+                case "StartField08" : list.add(11108);
                     break;
                     
-                case "StandardFied":
+                case "RepairSite01" : list.add(10901);
+                    break;
+                case "RepairSite02" : list.add(10902);
+                    break;
+                    
+                case "StandardFied": list.add(11000);
                     break;
                     
                 default : break;
