2c41681d751b498db034219a9af67314ff68153e
Uxa, Marc Andre <marc_andre.uxa@stud.htwk-leipzig.de>
2022-01-05 23:21:51 +0100

    deleted unused imports

--- code/core/src/htwk/mechawars/OptionScreen.java
+++ code/core/src/htwk/mechawars/OptionScreen.java
@@ -23,7 +23,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import htwk.mechawars.game.GameScreen;
