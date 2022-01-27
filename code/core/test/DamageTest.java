5c9e2b0e07a6375756b5c75a6011c7b907b6af5f
tkretzsc <tim_michael.kretzschmar@stud.htwk-leipzig.de>
2022-01-27 19:03:31 +0100

    error???

--- code/core/test/DamageTest.java
+++ code/core/test/DamageTest.java
@@ -1,11 +1,8 @@
 import htwk.mechawars.board.Board;
 import htwk.mechawars.board.Dir;
 import htwk.mechawars.board.Robot;
-import htwk.mechawars.cards.Card;
-import htwk.mechawars.cards.Type;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.LinkedList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
